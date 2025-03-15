6c83319a553d65fd00b4a8587831488feef0fe61
Angry Coding <mailidanielifechukwu@gmail.com>
2025-03-15 09:38:38 +0000

    chore: auth stuff

--- src/services/auth/index.ts
+++ src/services/auth/index.ts
@@ -1,10 +1,5 @@
-import {
-  GoogleAuthProvider,
-  GithubAuthProvider,
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-  signInWithPopup,
-} from "firebase/auth";
+"use server";
+
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
@@ -13,91 +8,12 @@ import { FirestoreAdapter } from "@auth/firebase-adapter";
 import type { APIResponse } from "@/types";
 import { db } from "@/services/firebase";
 
-export async function signInWithGoogle() {
-  const provider = new GoogleAuthProvider();
-
-  try {
-    const userCreds = await signInWithPopup(auth, provider);
-    const idToken = await userCreds.user.getIdToken();
-
-    const response = await fetch("/api/auth/sign-in", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ idToken }),
-    });
-
-    const resBody = (await response.json()) as APIResponse<string>;
-    return response.ok && resBody.success;
-  } catch (error) {
-    console.error("Error signing in with Google:", error);
-    return false;
-  }
-}
-
-export async function signInWithGitHub() {
-  const provider = new GithubAuthProvider();
-
-  try {
-    const userCreds = await signInWithPopup(auth, provider);
-    const idToken = await userCreds.user.getIdToken();
-
-    const response = await fetch("/api/auth/sign-in", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ idToken }),
-    });
-
-    const resBody = (await response.json()) as APIResponse<string>;
-    return response.ok && resBody.success;
-  } catch (error) {
-    console.error("Error signing in with GitHub:", error);
-    return false;
-  }
-}
-
-export async function signUpWithEmail(email: string, password: string) {
-  try {
-    const userCreds = await createUserWithEmailAndPassword(
-      auth,
-      email,
-      password
-    );
-    const idToken = await userCreds.user.getIdToken();
-
-    const response = await fetch("/api/auth/sign-up", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ idToken }),
-    });
-
-    const resBody = (await response.json()) as APIResponse<string>;
-    return response.ok && resBody.success;
-  } catch (error) {
-    console.error("Error signing up with email:", error);
-    return false;
-  }
-}
-
-export async function signInWithEmail(email: string, password: string) {
-  try {
-    const userCreds = await signInWithEmailAndPassword(auth, email, password);
-    const idToken = await userCreds.user.getIdToken();
-
-    const response = await fetch("/api/auth/sign-in", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ idToken }),
-    });
-
-    const resBody = (await response.json()) as APIResponse<string>;
-    return response.ok && resBody.success;
-  } catch (error) {
-    console.error("Error signing in with email:", error);
-    return false;
-  }
-}
-
-export const { handlers, signIn, signOut, auth } = NextAuth({
+export const {
+  handlers,
+  signIn,
+  signOut: signOutFn,
+  auth,
+} = NextAuth({
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID as string,
@@ -110,3 +26,60 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
   ],
   adapter: FirestoreAdapter(db),
 });
+
+export async function signInWithGoogle() {
+  await signIn("google", { redirectTo: "/dashboard" });
+  return true;
+}
+
+export async function signInWithGitHub() {
+  await signIn("github", { redirectTo: "/dashboard" });
+  return true;
+}
+
+export async function signUpWithEmail(email: string, password: string) {
+  console.log("signUpWithEmail", email, password);
+  return true;
+  // try {
+  //   const userCreds = await createUserWithEmailAndPassword(
+  //     auth,
+  //     email,
+  //     password
+  //   );
+  //   const idToken = await userCreds.user.getIdToken();
+  //   const response = await fetch("/api/auth/sign-up", {
+  //     method: "POST",
+  //     headers: { "Content-Type": "application/json" },
+  //     body: JSON.stringify({ idToken }),
+  //   });
+  //   const resBody = (await response.json()) as APIResponse<string>;
+  //   return response.ok && resBody.success;
+  // } catch (error) {
+  //   console.error("Error signing up with email:", error);
+  //   return false;
+  // }
+}
+
+export async function signInWithEmail(email: string, password: string) {
+  console.log("signInWithEmail", email, password);
+  return true;
+  // try {
+  //   const userCreds = await signInWithEmailAndPassword(auth, email, password);
+  //   const idToken = await userCreds.user.getIdToken();
+  //   const response = await fetch("/api/auth/sign-in", {
+  //     method: "POST",
+  //     headers: { "Content-Type": "application/json" },
+  //     body: JSON.stringify({ idToken }),
+  //   });
+  //   const resBody = (await response.json()) as APIResponse<string>;
+  //   return response.ok && resBody.success;
+  // } catch (error) {
+  //   console.error("Error signing in with email:", error);
+  //   return false;
+  // }
+}
+
+export async function signOut() {
+  await signOutFn();
+  return true;
+}
