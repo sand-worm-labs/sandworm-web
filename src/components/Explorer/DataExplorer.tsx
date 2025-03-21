9d35dd8487f39432e2c52b28a10a019c5f02d7f1
ceeriil <simonorugbo15@gmail.com>
2025-03-21 15:50:53 +0100

    modify entities explorer

--- src/components/Explorer/DataExplorer.tsx
+++ src/components/Explorer/DataExplorer.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Database, EllipsisVertical, FileUp, Plus } from "lucide-react";
-import { useSearchParams, useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
@@ -15,25 +15,32 @@ import {
   DropdownMenuPortal,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { explorerMockData } from "@/_mockdata/explorer";
+import { explorerMockData, chains } from "@/_mockdata/explorer";
+import type { Chain, DataExplorer } from "@/_mockdata/explorer";
+
 import { ChainExplorer } from "./ChainExplorer";
 import { EntitiesExplorer } from "./EntitiesExplorer";
-import FieldExplorer from "./FieldExplorer";
 
 export default function DataExplorer() {
   const [isSheetOpen, setIsSheetOpen] = useState(false);
   const [searchTerm, setSearchTerm] = useState("");
-  const searchParams = useSearchParams();
   const router = useRouter();
+  const searchParams = useSearchParams();
+  const selectedChain = searchParams.get("namespace");
 
   const isLoading = false;
-
-  const namespace = searchParams.get("namespace"); // Chain (e.g., solana)
-  const id = searchParams.get("id"); // Entity (e.g., solana.transactions)
 
   const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearchTerm(e.target.value);
   };
+
+  const handleSelectChain = (chainId: string) => {
+    router.push(`?namespace=${chainId}`);
+  };
+
+  const selectedChainData: DataExplorer | undefined = explorerMockData.find(
+    chain => chain.chainId === selectedChain
+  );
 
   return (
     <Card className="h-full overflow-hidden border-none dark">
@@ -86,12 +93,12 @@ export default function DataExplorer() {
               </p>
             </div>
             <ul className="ml-2">
-              {!namespace ? (
-                <ChainExplorer chains={explorerMockData} />
-              ) : !id ? (
-                <EntitiesExplorer namespace={namespace} />
+              {selectedChain ? (
+                <EntitiesExplorer
+                  entities={selectedChainData?.entities || []}
+                />
               ) : (
-                <FieldExplorer namespace={namespace} id={id} />
+                <ChainExplorer chains={chains} onSelect={handleSelectChain} />
               )}
             </ul>
           </div>
