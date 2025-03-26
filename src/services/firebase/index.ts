d2d84ed01017ad30f5ed90b062a6d18b4e780f25
ceeriil <simonorugbo15@gmail.com>
2025-03-26 07:20:03 +0100

    edit theme

--- src/services/firebase/index.ts
+++ src/services/firebase/index.ts
@@ -3,17 +3,13 @@ import { getFirestore } from "firebase-admin/firestore";
 import { getAuth } from "firebase-admin/auth";
 
 if (process.env.NODE_ENV === "test") {
-  // We won't be using firebase for testing for now. At some point,
-  // we might want to run tests against the Staging firebase instance.
-  // throw new Error(
-  //   ` This will connect to the production firestore.
-  //     Make sure db/firebase.ts is updated before testing against Firebase`
-  // );
+  console.log("initialize");
   admin.initializeApp({
     projectId: "sandworm-8aa45",
     storageBucket: "sandworm-8aa45.appspot.com",
   });
 }
+
 if (!admin.apps.length && process.env.NODE_ENV === "development") {
   if (process.env.FIRESTORE_EMULATOR_HOST) {
     console.log("using Firebase **emulator** DB");
@@ -25,12 +21,15 @@ if (!admin.apps.length && process.env.NODE_ENV === "development") {
 
     // seedDatabase();
   } else {
+    console.log("initialize 2");
+
     admin.initializeApp({
       storageBucket: "sandworm-8aa45.appspot.com",
     });
   }
 } else if (!admin.apps.length && process.env.GOOGLE_APPLICATION_CREDENTIALS) {
   console.log("using Firebase live DB");
+  console.log("initialize 3");
   admin.initializeApp({
     credential: admin.credential.applicationDefault(),
     storageBucket: "sandworm-8aa45.appspot.com",
