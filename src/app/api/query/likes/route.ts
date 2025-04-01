7c5e05b92544eec285aefd1a8eff8496a4e1ab4f
Angry Coding <mailidanielifechukwu@gmail.com>
2025-04-01 23:03:34 +0000

    chore: fork, likes, updates, user

--- src/app/api/query/likes/route.ts
+++ src/app/api/query/likes/route.ts
@@ -2,14 +2,28 @@ import "server-only";
 import "@/services/firebase";
 
 import { QueryService } from "@/services/firebase/db/QueryService";
+import { DataResult } from "@/services/firebase/db";
 
-export async function PATCH(
-  request: Request,
-  { params }: { params: { queryId: string } }
-) {
-  // const url = new URL(request.url);
-  // const uid = url.searchParams.get("uid");
-  // const result = await QueryService.findAllUserQuery(uid || "");
-  // if (!result.success) return new Response(JSON.stringify(result), { status: 500 });
-  // return new Response(JSON.stringify(result.data));
+export async function PATCH(request: Request) {
+  const {
+    queryId,
+    uid,
+    like,
+  }: { queryId: string; uid: string; like: boolean } = await request.json();
+  if (!queryId || !uid)
+    return new Response(
+      JSON.stringify(DataResult.failure("Missing query ID.", "MISSING_FIELDS")),
+      { status: 400 }
+    );
+  if (like) {
+    const result = await QueryService.like(queryId, uid);
+    if (!result.success)
+      return new Response(JSON.stringify(result), { status: 500 });
+    return new Response(JSON.stringify(result.data));
+  }
+
+  const result = await QueryService.unlike(queryId, uid);
+  if (!result.success)
+    return new Response(JSON.stringify(result), { status: 500 });
+  return new Response(JSON.stringify(result.data));
 }
