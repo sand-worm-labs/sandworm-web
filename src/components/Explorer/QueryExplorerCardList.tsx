fabe9e251c98906af47d8aac856b94198ceca942
ceeriil <simonorugbo15@gmail.com>
2025-04-02 19:05:14 +0100

    change

--- src/components/Explorer/QueryExplorerCardList.tsx
+++ src/components/Explorer/QueryExplorerCardList.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { AiOutlineCode } from "react-icons/ai";
 
 import { Input } from "../ui/input";
+import { Search } from "lucide-react";
 
 interface IQuery {
   id: string;
@@ -16,22 +17,28 @@ export const QueryExplorerCardList: React.FC<IQueryExplorerCardListProps> = ({
   query,
 }) => {
   return (
-    <div className="px-2 py-4">
-      <Input
-        type="text"
-        placeholder="Find Query..."
-        className="m-auto w-full focus:ring-0"
-      />
-      <ul className="flex flex-col gap-2 p-4 border w-full mt-4">
+    <div className=" py-4">
+      <div className="relative w-full px-3">
+        <Search
+          className="absolute left-5 top-1/2 -translate-y-1/2 text-text-gray"
+          size={16}
+        />
+        <Input
+          type="text"
+          placeholder="Search Queries"
+          className="pl-10 pr-4 py-1.5 border border-[#ffffff60] focus:border-gray-500 focus:ring focus:ring-gray-300 md:text-[0.85rem]  bg-[#1A1A1A] border-none w-full rounded-none"
+        />
+      </div>
+      <ul className="flex flex-col    w-full my-4 ">
         {query.map(item => (
           <button
             key={item.id}
-            className="cursor-pointer py-2 px-2 rounded-md hover:bg-primary/10 text-sm text-left flex items-center space-x-2 lowercase font-medium"
+            className="cursor-pointer p-3 border-b  hover:bg-white/10 text-sm text-left flex items-center space-x-3 lowercase font-medium py-5  first:border-t "
             type="button"
           >
-            <AiOutlineCode size={15} />
+            <AiOutlineCode size={18} />
 
-            <span className="text-sm font-medium ">{item.name}</span>
+            <span className="text-sm capitalize ">{item.name}</span>
           </button>
         ))}
       </ul>
