a5cf56f84f69a7dcf092844a7e70cb7d2a1806c6
ceeriil <simonorugbo15@gmail.com>
2025-04-03 07:42:24 +0100

    remove option to run query in editor

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -2,14 +2,14 @@ import MainLayout from "@/layouts/MainLayout/MainLayout";
 import { SectionFeatures } from "@/components/Sections/SectionFeatures";
 import { SectionHero } from "@/components/Sections/SectionHero";
 import { SectionOnboard } from "@/components/Sections/SectionOnboard";
-import { SectionWorkflow } from "@/components/Sections/SectionWorkFlow";
+import { SectionWorkFlow } from "@/components/Sections/SectionWorkFlow";
 
 export default function Home() {
   return (
     <MainLayout>
       <SectionHero />
       <SectionFeatures />
-      <SectionWorkflow />
+      <SectionWorkFlow />
       <SectionOnboard />
     </MainLayout>
   );
