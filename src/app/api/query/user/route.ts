529f5665e915d43c76c6c804ff444b2d4265145a
Angry Coding <mailidanielifechukwu@gmail.com>
2025-04-05 12:26:06 +0000

    chore: get user query and profile

--- src/app/api/query/user/route.ts
+++ src/app/api/query/user/route.ts
@@ -2,29 +2,10 @@ import "server-only";
 import "@/services/firebase";
 
 import { QueryService } from "@/services/firebase/db/QueryService";
-import { DataResult } from "@/services/firebase/db";
-
-import { auth } from "../../auth/[...nextauth]/auth-options";
 
 export async function GET(request: Request) {
-  const session = await auth();
-
-  if (!session) {
-    return new Response(
-      JSON.stringify(DataResult.failure("Unauthorized", "UNAUTHORIZED")),
-      { status: 401 }
-    );
-  }
-
   const url = new URL(request.url);
   const uid = url.searchParams.get("uid");
-
-  if (session.user?.id !== uid) {
-    return new Response(
-      JSON.stringify(DataResult.failure("Invalid User", "UNAUTHORIZED")),
-      { status: 401 }
-    );
-  }
 
   const result = await QueryService.findAllUserQuery(uid || "");
   if (!result.success)
