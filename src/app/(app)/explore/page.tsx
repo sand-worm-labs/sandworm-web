a6cf35b798deb36e16177cc410957ed1c7f7d777
ceeriil <simonorugbo15@gmail.com>
2025-04-05 02:51:34 +0100

    feat: display queries

--- src/app/(app)/explore/page.tsx
+++ src/app/(app)/explore/page.tsx
@@ -1,15 +1,32 @@
 import Head from "next/head";
 
-import TabsSection from "@/components/TabsSection";
+import { AxiosService } from "@/services/axios";
+import type { Query } from "@/types";
+
+import { TabsSection } from "@/components/TabsSection";
+
+const axios = new AxiosService(process.env.NEXT_PUBLIC_API_URL!, false);
+
+async function getQueries(): Promise<Query[]> {
+  try {
+    const queries = await axios.get<Query[]>("/api/query");
+    return queries;
+  } catch (err) {
+    console.error("failed to fetch queries:", err);
+    return [];
+  }
+}
 
 export default async function Explore() {
+  const queries = await getQueries();
+
   return (
     <div className="dark text-white min-h-screen">
       <Head>
         <title>Explore</title>
       </Head>
       <div className="pt-10">
-        <TabsSection />
+        <TabsSection queries={queries} />
       </div>
     </div>
   );
