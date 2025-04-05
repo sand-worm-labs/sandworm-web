9ff977c75206aa129765885f12cb3601ac4603c3
ceeriil <simonorugbo15@gmail.com>
2025-04-05 17:09:54 +0100

    removed onscroll

--- src/app/(app)/creators/[id]/page.tsx
+++ src/app/(app)/creators/[id]/page.tsx
@@ -1,7 +1,27 @@
-"use client";
-
 import { Creators } from "@/components/Creators";
+import { notFound } from "next/navigation";
+import type { QueryResponse } from "@/types";
+import { AxiosService } from "@/services/axios";
 
-export default function CreatorsPage() {
-  return <Creators />;
+const axios = new AxiosService(process.env.NEXT_PUBLIC_API_URL!, false);
+
+interface ExplorePageProps {
+  searchParams: {
+    page?: string;
+  };
+}
+
+async function getQueries(page: string = "1"): Promise<QueryResponse> {
+  const queries = await axios.get<QueryResponse>(
+    `/api/query/?page=${page}&limit=100`
+  );
+  if (!queries) notFound();
+  return queries;
+}
+
+export default async function CreatorsPage({ searchParams }: ExplorePageProps) {
+  const page = searchParams.page ?? "1";
+  const queries = await getQueries(page);
+
+  return <Creators queries={queries} />;
 }
