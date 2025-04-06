689ecd6ad51146bf628bbe2d20e4cfa58dcd6b19
Angry Coding <mailidanielifechukwu@gmail.com>
2025-04-06 15:18:00 +0000

    chore: added stared by route

---
+++ src/app/api/query/star/route.ts
@@ -0,0 +1,34 @@
+import "server-only";
+import "@/services/firebase";
+
+import { QueryService } from "@/services/firebase/db/QueryService";
+import { DataResult, getPaginationDetails, toPaginatedResult } from "@/services/firebase/db";
+import { UserService } from "@/services/firebase/db/users";
+
+export async function GET(request: Request) {
+  const url = new URL(request.url);
+  const uid = url.searchParams.get("uid");
+
+  if (!uid) {
+    return new Response(
+      JSON.stringify(DataResult.failure("Missing UID", "BAD_REQUEST")),
+      { status: 400 }
+    );
+  }
+
+  const userResult = await UserService.findUserById(uid);
+  if (!userResult.success) {
+    return new Response(JSON.stringify(userResult), { status: 404 });
+  }
+
+  const staredResult = await QueryService.findAllUserStaredQuery(uid);
+
+  const { totalRecords, totalPages, currentPage, nextPage, prevPage } = getPaginationDetails(0, 10, 1);
+  const emptyResult = toPaginatedResult([], totalRecords, currentPage, totalPages, nextPage, prevPage);
+
+
+  return new Response(
+    JSON.stringify(staredResult.success ? staredResult.data :[]),
+    { status: 200 }
+  );
+}
