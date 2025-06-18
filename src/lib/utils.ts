3289bbe1e7ee03679292641226fe7ab00cd07fe1
ceeriil <simonorugbo15@gmail.com>
2025-06-18 19:54:19 +0100

    components for chat

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,6 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+// eslint-disable-next-line no-bitwise
+export const keyCombo = (mod: number, key: number) => mod | key;
