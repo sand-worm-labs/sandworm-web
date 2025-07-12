f45af9bea44def433c6ffb657ac1391b84fad6aa
ceeriil <simonorugbo15@gmail.com>
2025-07-12 11:53:01 +0100

    chart

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -1,10 +1,22 @@
+"use client";
 import { ResearchDescription } from "@/components/Dashboard/DashboardDescriptin";
 import { DashboardHeader } from "@/components/Dashboard/DashboardHeader";
 import { CounterCard } from "@/components/WorkSpace/ResultTab/Charts/Counter";
+import { PieChart } from "@/components/WorkSpace/ResultTab/Charts/PieChart";
 
 import React from "react";
 
 export default function Dashboard() {
+  const gmxPieChartData = {
+    columns: ["destination", "amount"],
+    data: [
+      ["Bridged to Ethereum", 9600000],
+      ["Swapped to ETH", 32000000],
+      ["Returned via Bounty", 2000000],
+      ["Still on Arbitrum", 2000000],
+      ["Unknown", 300000],
+    ],
+  };
   return (
     <div className="container mx-auto  mt-12">
       <DashboardHeader />
@@ -39,6 +51,10 @@ export default function Dashboard() {
             description="Swapped by attacker post‑bridging"
           />
         </div>
+        <PieChart
+          result={gmxPieChartData}
+          title="Funds Distribution Post‑GMX Exploit"
+        />
       </div>
     </div>
   );
