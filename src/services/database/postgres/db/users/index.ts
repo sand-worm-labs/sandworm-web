8e8ab8eb557f0f35d02204797a3a00dfc60d36ed
Ifechukwudaniel <mailidanielifechukwu@gmail.com>
2025-09-18 11:25:21 +0000

    chore: delete db folder
    Signed-off-by: Ifechukwudaniel <mailidanielifechukwu@gmail.com>

--- src/services/database/postgres/db/users/index.ts
+++
@@ -1,4 +0,0 @@
-export { UserService } from "./UserService";
-export { SessionService } from "./SessionService";
-export { AccountService } from "./AccountService";
-export { VerificationService } from "./VerificationService";
