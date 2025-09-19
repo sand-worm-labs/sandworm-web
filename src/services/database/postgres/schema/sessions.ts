811862762946dd809a25db3548c2dd090ffd4516
Angry Coding <mailidanielifechukwu@gmail.com>
2025-09-19 22:26:49 +0000

    chore: accounts , chats , sessions

--- src/services/database/postgres/schema/sessions.ts
+++ src/services/database/postgres/schema/sessions.ts
@@ -1,4 +1,5 @@
 import { pgTable, varchar, timestamp } from "drizzle-orm/pg-core";
+import { z } from "zod";
 
 // Session table
 export const sessions = pgTable("sessions", {
@@ -6,3 +7,11 @@ export const sessions = pgTable("sessions", {
   userId: varchar("user_id", { length: 255 }).notNull(),
   expires: timestamp("expires", { mode: "date" }).notNull(),
 });
+
+export const sessionSchema = z.object({
+  sessionToken: z.string().max(255),
+  userId: z.string().max(255),
+  expires: z.date(), // since Drizzle timestamp({ mode: "date" }) → JS Date
+});
+
+export type Session = z.infer<typeof sessionSchema>;
