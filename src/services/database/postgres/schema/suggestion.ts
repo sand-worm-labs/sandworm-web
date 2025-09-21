eba6d937c1cd25e907442e7de44db6ff3ca0a43f
Ifechukwudaniel <mailidanielifechukwu@gmail.com>
2025-09-21 01:57:35 +0000

    chore: schema definations
    Signed-off-by: Ifechukwudaniel <mailidanielifechukwu@gmail.com>

---
+++ src/services/database/postgres/schema/suggestion.ts
@@ -0,0 +1,52 @@
+import {
+  boolean,
+  foreignKey,
+  pgTable,
+  primaryKey,
+  text,
+  timestamp,
+  uuid,
+} from "drizzle-orm/pg-core";
+import { z } from "zod";
+
+import { user } from "./user";
+import { document } from "./document";
+
+export const suggestion = pgTable(
+  "Suggestion",
+  {
+    id: uuid("id").notNull().defaultRandom(),
+    documentId: uuid("documentId").notNull(),
+    documentCreatedAt: timestamp("documentCreatedAt").notNull(),
+    originalText: text("originalText").notNull(),
+    suggestedText: text("suggestedText").notNull(),
+    description: text("description"),
+    isResolved: boolean("isResolved").notNull().default(false),
+    userId: uuid("userId")
+      .notNull()
+      .references(() => user.id),
+    createdAt: timestamp("createdAt").notNull(),
+  },
+  table => ({
+    pk: primaryKey({ columns: [table.id] }),
+    documentRef: foreignKey({
+      columns: [table.documentId, table.documentCreatedAt],
+      foreignColumns: [document.id, document.createdAt],
+    }),
+  })
+);
+
+// ============== Suggestion Schema Type ==============
+export const suggestionSchema = z.object({
+  id: z.string().uuid(),
+  documentId: z.string().uuid(),
+  documentCreatedAt: z.date(),
+  originalText: z.string(),
+  suggestedText: z.string(),
+  description: z.string().nullable().optional(),
+  isResolved: z.boolean().default(false),
+  userId: z.string().uuid(),
+  createdAt: z.date(),
+});
+
+export type Suggestion = z.infer<typeof suggestionSchema>;
