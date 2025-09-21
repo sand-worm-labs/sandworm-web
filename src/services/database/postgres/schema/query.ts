da9bb992e33fad767dc9d5f29b775c1c6f429354
Ifechukwudaniel <mailidanielifechukwu@gmail.com>
2025-09-21 04:24:00 +0000

    chore:  docker and seed
    Signed-off-by: Ifechukwudaniel <mailidanielifechukwu@gmail.com>

---
+++ src/services/database/postgres/schema/query.ts
@@ -0,0 +1,52 @@
+import {
+  pgTable,
+  text,
+  jsonb,
+  integer,
+  boolean,
+  timestamp,
+  uuid,
+} from "drizzle-orm/pg-core";
+import { z } from "zod";
+
+import { UserTable } from "./user";
+
+// Zod schema for typing
+export const QueryType = z.object({
+  title: z.string(),
+  description: z.string(),
+  creator: z.string(),
+  private: z.boolean(),
+  query: z.string(),
+  tags: z.array(z.string()),
+  stars: z.number(),
+  forks: z.number(),
+  stared_by: z.array(z.string()),
+  forked_from: z.string().nullable(),
+  forked_by: z.array(z.string()),
+  forked: z.boolean(),
+  createdAt: z.date(),
+  updatedAt: z.date(),
+});
+
+export type Query = z.infer<typeof QueryType>;
+
+export const QueryTable = pgTable("Query", {
+  id: uuid("id").primaryKey().defaultRandom(),
+  title: text("title").notNull(),
+  description: text("description").notNull(),
+  creator: uuid("creator").notNull(), // foreign key to user.id
+  private: boolean("private").notNull().default(false),
+  query: text("query").notNull(),
+  tags: jsonb("tags").notNull().default([]),
+  stared_by: jsonb("stared_by").notNull().default([]),
+  forked_from: uuid("forked_from")
+    .default("")
+    .references(() => UserTable.id),
+  forked_by: jsonb("forked_by").notNull().default([]),
+  forked: boolean("forked").notNull().default(false),
+  stars: integer("stars").notNull().default(0),
+  forks: integer("forks").notNull().default(0),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at").defaultNow().notNull(),
+});
