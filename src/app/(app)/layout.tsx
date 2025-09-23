2c5a3064b20daa16188f372ad80854edd00e8ee4
ceeriil <simonorugbo15@gmail.com>
2025-09-23 17:19:51 +0100

    ui: switch workspace typography to roobert font

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -9,23 +9,25 @@ import { SignInModal } from "@/components/AuthUI/SignInModal";
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
-    <ProgressProvider
-      height="1px"
-      color="#FF7F4F"
-      options={{ showSpinner: false }}
-      shallowRouting
-    >
-      <AppHeader />
-      <main>{children}</main>
-      <FooterWrapper />
-      <SignInModal />
-      <Toaster
-        position="bottom-right"
-        toastOptions={{
-          className:
-            " border border-neutral-700 shadow-xl rounded-none dm-sans ",
-        }}
-      />
-    </ProgressProvider>
+    <div className="roobert">
+      <ProgressProvider
+        height="1px"
+        color="#FF7F4F"
+        options={{ showSpinner: false }}
+        shallowRouting
+      >
+        <AppHeader />
+        <main> {children}</main>
+        <FooterWrapper />
+        <SignInModal />
+        <Toaster
+          position="bottom-right"
+          toastOptions={{
+            className:
+              " border border-neutral-700 shadow-xl rounded-none dm-sans ",
+          }}
+        />
+      </ProgressProvider>
+    </div>
   );
 }
