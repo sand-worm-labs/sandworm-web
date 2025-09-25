90c51545eacffd7c49168db413526e9470a5f7bd
Ifechukwudaniel <mailidanielifechukwu@gmail.com>
2025-09-25 21:39:54 +0000

    feat: database packages
    Signed-off-by: Ifechukwudaniel <mailidanielifechukwu@gmail.com>

---
+++ packages/database/src/schemas/index.ts
@@ -0,0 +1,11 @@
+// NEXT AUTH  TABLES
+export * from "./nextauth";
+export * from "./user";
+
+export * from "./chat";
+export * from "./query";
+export * from "./vote";
+export * from "./document";
+export * from "./suggestion";
+export * from "./stream";
+export * from "./message";
