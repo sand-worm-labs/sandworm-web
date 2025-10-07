2807c268c89d5a317defc391f8841fdf05f55cb8
Ifechukwudaniel <mailidanielifechukwu@gmail.com>
2025-10-07 12:13:03 +0000

    chore: refactor
    Signed-off-by: Ifechukwudaniel <mailidanielifechukwu@gmail.com>

--- packages/database/src/types/db.ts
+++ packages/database/src/types/db.ts
@@ -1,7 +1,7 @@
 import type { NeonDatabase } from "drizzle-orm/neon-serverless";
-
-import type * as schema from "./schemas";
+import type * as schema from "../schemas";
 
 export type SandwormDatabaseSchema = typeof schema;
 
 export type SandwormDatabase = NeonDatabase<SandwormDatabaseSchema>;
+
