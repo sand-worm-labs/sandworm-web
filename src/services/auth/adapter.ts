71b18d893cc1fe2cf560ae02bc3aafc93c04a9f8
ceeriil <simonorugbo15@gmail.com>
2025-10-07 07:19:28 +0100

    🐛fix: fix imports on db package

--- src/services/auth/adapter.ts
+++ src/services/auth/adapter.ts
@@ -1,7 +1,7 @@
 import { DrizzleAdapter } from "@auth/drizzle-adapter";
 
 import {
-  UserTable,
+  users,
   nextauthAccounts,
   nextauthSessions,
   nextauthVerificationTokens,
@@ -9,7 +9,7 @@ import {
 import { db } from "@sandworm/database/db";
 
 export const AuthAdapter = DrizzleAdapter(db, {
-  usersTable: UserTable,
+  usersTable: users,
   accountsTable: nextauthAccounts,
   sessionsTable: nextauthSessions,
   verificationTokensTable: nextauthVerificationTokens,
