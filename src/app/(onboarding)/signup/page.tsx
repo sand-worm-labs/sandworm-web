cbaa7c8384fb3842d6b5516b70f754653c3d2896
ceeriil <simonorugbo15@gmail.com>
2025-10-08 14:15:53 +0100

    🖼 ui: update onboarding layout

--- src/app/(onboarding)/signup/page.tsx
+++ src/app/(onboarding)/signup/page.tsx
@@ -1,10 +1,12 @@
 import { ClaimUsername } from "@/components/AuthUI/ClaimUsername";
+import { SignUp } from "@/components/AuthUI/SignUp";
 
 export default function SignUpPage() {
   return (
     <div>
       {/*       <SignUp />
        */}{" "}
+      <SignUp />
       <ClaimUsername />
     </div>
   );
