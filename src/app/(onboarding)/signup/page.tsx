d84a8db4d266fa14df351ebab272bef8d2b3d9c2
ceeriil <simonorugbo15@gmail.com>
2025-10-09 07:31:03 +0100

    🖼 ui: integrate stepper component in onboarding page

--- src/app/(onboarding)/signup/page.tsx
+++ src/app/(onboarding)/signup/page.tsx
@@ -1,13 +1,18 @@
-import { ClaimUsername } from "@/components/AuthUI/ClaimUsername";
 import { SignUp } from "@/components/AuthUI/SignUp";
+import { Stepper } from "@/components/AuthUI/Stepper";
 
 export default function SignUpPage() {
   return (
     <div>
-      {/*       <SignUp />
-       */}{" "}
+      <Stepper
+        currentStep={0}
+        steps={[
+          { name: "Sign Up", href: "/signup" },
+          { name: "Setup Subdomain", href: "/claim" },
+        ]}
+      />
+
       <SignUp />
-      <ClaimUsername />
     </div>
   );
 }
