5bf5edb7458ac043127d6c08ce569daa4ff58439
Ifechukwudaniel <mailidanielifechukwu@gmail.com>
2025-10-10 05:42:09 +0000

    chore: refactore
    Signed-off-by: Ifechukwudaniel <mailidanielifechukwu@gmail.com>

--- apps/api/src/users/entities/user.entity.ts
+++ apps/api/src/users/entities/user.entity.ts
@@ -1,7 +1,37 @@
-import { ObjectType, Field, Int } from "@nestjs/graphql";
+import { ObjectType, Field, ID } from "@nestjs/graphql";
+import { InferSelectModel } from "drizzle-orm";
+import { users } from "@sandworm/database/src/schemas";
 
 @ObjectType()
-export class User {
-  @Field(() => Int, { description: "Example field (placeholder)" })
-  exampleField: number;
+export class User implements InferSelectModel<typeof users> {
+  accessedAt: Date;
+  @Field(() => ID)
+  id: string;
+
+  @Field({ nullable: true })
+  username: string | null;
+
+  @Field({ nullable: true })
+  email: string | null;
+
+  @Field({ nullable: true })
+  firstName: string | null;
+
+  @Field({ nullable: true })
+  lastName: string | null;
+
+  @Field({ nullable: true })
+  fullName: string | null;
+
+  @Field({ nullable: true })
+  avater: string | null;
+
+  emailVerifiedAt: Date | null;
+  emailVerified: boolean;
+
+  @Field(() => Date)
+  createdAt: Date;
+
+  @Field(() => Date)
+  updatedAt: Date;
 }
