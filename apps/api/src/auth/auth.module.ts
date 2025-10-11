bed0e89a2b8b645b7769cb26177fe9e94e132e87
Ifechukwudaniel <mailidanielifechukwu@gmail.com>
2025-10-11 09:03:41 +0000

    chore: auth stuff
    Signed-off-by: Ifechukwudaniel <mailidanielifechukwu@gmail.com>

--- apps/api/src/auth/auth.module.ts
+++ apps/api/src/auth/auth.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { UserEntity } from '@repo/postgresql-typeorm';
+// import { TypeOrmModule } from '@nestjs/typeorm';
+// import { UserEntity } from '@repo/postgresql-typeorm';
 import { AuthResolver } from './auth.resolver';
 import { AuthService } from './auth.service';
 
 @Module({
-  imports: [JwtModule.register({}), TypeOrmModule.forFeature([UserEntity])],
+  imports: [JwtModule.register({})],
   providers: [AuthResolver, AuthService],
   exports: [AuthService],
 })
-export class AuthModule {}
+export class AuthModule { }
