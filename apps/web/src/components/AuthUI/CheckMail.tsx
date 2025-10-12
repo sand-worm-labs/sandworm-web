61508b813db8378fcfc869a6d4f94afe911838fd
ibrahimsunday <ibrahimsundayijai@gmail.com>
2025-10-12 14:32:17 +0100

    fix: fixed hardcoded "muted texts"

--- apps/web/src/components/AuthUI/CheckMail.tsx
+++ apps/web/src/components/AuthUI/CheckMail.tsx
@@ -9,7 +9,7 @@ export const CheckMail = () => {
         <Image src="/img/mail.png" width={24} height={24} alt="mail" />
       </div>
       <h2 className="text-xl font-semibold roobert">Check your Mail!</h2>
-      <p className="text-sm font-medium text-[#70767B] dark:text-white roobert max-w-md mt-2">
+      <p className="text-sm font-medium text-muted-foreground dark:text-white roobert max-w-md mt-2">
         Congrats! Please click the link sent to your{" "}
         <span className="font-semibold">email</span> to complete your account
         creation process.
@@ -18,7 +18,7 @@ export const CheckMail = () => {
         Did not get an email?{" "}
         <span className="text-[#8053FE] underline cursor-pointer">Resend</span>
       </p>
-      <p className="roobert text-center text-xs text-[#6C757D] mt-6">
+      <p className="roobert text-center text-xs text-muted-foreground mt-6">
         By creating an account you agree to the{" "}
         <span className="underline">Terms</span> and confirm that you have read
         the <span className="underline">Privacy Policy</span>.
