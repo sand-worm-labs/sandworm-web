118993524dcf75886e6e1e5cf556dd2c1ee430c2
Ifechukwudaniel <mailidanielifechukwu@gmail.com>
2025-10-15 07:25:23 +0000

    chore: schema fixes

---
+++ packages/postgresql-typeorm/src/entities/vote.entity.ts
@@ -0,0 +1,56 @@
+import {
+    Entity,
+    Column,
+    ManyToOne,
+    JoinColumn,
+    PrimaryColumn,
+} from "typeorm";
+import type { Relation } from "typeorm";
+import { UserEntity } from "./user.entity";
+import { ChatEntity } from "./chat.entity";
+import { MessageEntity } from "./message.entity";
+
+@Entity("votes")
+export class VoteEntity {
+    @PrimaryColumn("uuid", { name: "user_id" })
+    userId!: string;
+
+    @PrimaryColumn("uuid", { name: "chat_id" })
+    chatId!: string;
+
+    @PrimaryColumn("uuid", { name: "message_id" })
+    messageId!: string;
+
+    @Column({ name: "is_upvoted", type: "boolean", nullable: false })
+    isUpvoted!: boolean;
+
+    @ManyToOne(() => UserEntity, (user) => user.votes, {
+        onDelete: "CASCADE",
+    })
+    @JoinColumn({
+        name: "user_id",
+        referencedColumnName: "id",
+        foreignKeyConstraintName: "FK_votes_user",
+    })
+    user!: Relation<UserEntity>;
+
+    @ManyToOne(() => ChatEntity, (chat) => chat.votes, {
+        onDelete: "CASCADE",
+    })
+    @JoinColumn({
+        name: "chat_id",
+        referencedColumnName: "id",
+        foreignKeyConstraintName: "FK_votes_chat",
+    })
+    chat!: Relation<ChatEntity>;
+
+    @ManyToOne(() => MessageEntity, (message) => message.votes, {
+        onDelete: "CASCADE",
+    })
+    @JoinColumn({
+        name: "message_id",
+        referencedColumnName: "id",
+        foreignKeyConstraintName: "FK_votes_message",
+    })
+    message!: Relation<MessageEntity>;
+}
