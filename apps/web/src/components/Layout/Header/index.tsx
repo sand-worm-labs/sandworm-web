840c689becb9d4fca7173dd3e5100010d875b59f
ceeriil <simonorugbo15@gmail.com>
2025-10-17 00:35:21 +0100

    🔧chore: edit partners section

--- apps/web/src/components/Layout/Header/index.tsx
+++ apps/web/src/components/Layout/Header/index.tsx
@@ -8,9 +8,18 @@ import { Badge } from "@/components/ui/badge";
 
 const navLinks = [
   { name: "Explore", href: "workspace/explore" },
-  { name: "Docs", href: "https://docs.sandwormlabs.xyz", isExternal: true },
+  {
+    name: "Documentation",
+    href: "https://docs.sandwormlabs.xyz",
+    isExternal: true,
+  },
   {
     name: "Blog",
+    href: "https://docs.sandwormlabs.xyz/blog",
+    isExternal: true,
+  },
+  {
+    name: "About",
     href: "https://docs.sandwormlabs.xyz/blog",
     isExternal: true,
   },
@@ -21,37 +30,38 @@ export const MainHeader = () => {
 
   return (
     <header className="  fixed w-full top-5  mx-auto left-0 right-0 z-[99]">
-      <div className="w-[60%] mx-auto bg-[#FF887A] dark:bg-[#000000] rounded-xl">
+      <div className="w-[85%] mx-auto  rounded-xl">
         <div className="px-3 flex justify-between items-center py-2.5">
           <div className="flex">
             <Link href="/" className="flex items-center">
               <SandwormLogo />
-              <span className="ml-3 font-medium text-xl uppercase">
+              <span className="ml-3 font-medium text-xl uppercase text-white">
                 Sandw0rm.
               </span>
               <Badge className="bg-white text-black dark:text-white rounded-xl ml-2 font-normal">
                 beta
               </Badge>
             </Link>
-            <ul className="hidden md:flex ml-10 text-[0.8rem] items-center space-x-8">
-              {navLinks.map(link => (
-                <li key={link.name}>
-                  <Link
-                    href={link.href}
-                    {...(link.isExternal
-                      ? { target: "_blank", rel: "noopener noreferrer" }
-                      : {})}
-                    className="dark:text-[#999999] text-[#000000] font-bold uppercase  hover:text-[#C7665C]"
-                  >
-                    {link.name}
-                  </Link>
-                </li>
-              ))}
-            </ul>
           </div>
 
+          <ul className="hidden md:flex ml-10 text-[0.8rem] items-center space-x-8 rounded-full backdrop-blur-lg py-2.5 px-8 border border-borderLight">
+            {navLinks.map(link => (
+              <li key={link.name}>
+                <Link
+                  href={link.href}
+                  {...(link.isExternal
+                    ? { target: "_blank", rel: "noopener noreferrer" }
+                    : {})}
+                  className="text-neutral-500 font-medium  hover:text-[#C7665C]"
+                >
+                  {link.name}
+                </Link>
+              </li>
+            ))}
+          </ul>
+
           <Link
-            className="hidden md:flex border py-2 bg-black dark:bg-white text-white dark:text-black rounded-xl px-4 text-[0.9rem] font-medium"
+            className="hidden md:flex border py-2 bg-primary text-black rounded-2xl px-4 text-[0.9rem] font-medium"
             href="/workspace"
           >
             <span>Launch App</span>
