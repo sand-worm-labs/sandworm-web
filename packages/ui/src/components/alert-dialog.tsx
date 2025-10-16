124545082cc87c4eb7639e3a1a3ed13d58cb2ed3
ibrahimsunday <ibrahimsundayijai@gmail.com>
2025-10-16 02:33:51 +0100

    chore: using ui package for buttons in authuui

--- packages/ui/src/components/alert-dialog.tsx
+++ packages/ui/src/components/alert-dialog.tsx
@@ -3,8 +3,8 @@
 import * as React from "react"
 import * as AlertDialogPrimitive from "@radix-ui/react-alert-dialog"
 
-import { cn } from "@workspace/ui/lib/utils"
-import { buttonVariants } from "@workspace/ui/components/button"
+import { cn } from "@sandworm/ui/lib/utils"
+import { buttonVariants } from "@sandworm/ui/components/button";
 
 function AlertDialog({
   ...props
