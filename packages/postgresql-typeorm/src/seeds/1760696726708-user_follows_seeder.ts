fea54c04caecdd9c1e265f86b9a6e9fa13250490
Ifechukwudaniel <mailidanielifechukwu@gmail.com>
2025-10-17 11:29:44 +0000

    chore: update seeders to use DocumentEntity and add new seeder files

---
+++ packages/postgresql-typeorm/src/seeds/1760696726708-user_follows_seeder.ts
@@ -0,0 +1,13 @@
+import { DataSource } from 'typeorm';
+import { Seeder, SeederFactoryManager } from 'typeorm-extension';
+
+export class UserFollowsSeeder1760696726708 implements Seeder {
+    track = false;
+
+    public async run(
+        dataSource: DataSource,
+        factoryManager: SeederFactoryManager
+    ): Promise<any> {
+
+    }
+}
---
+++ packages/postgresql-typeorm/src/seeds/1760696739409-workspace_seeder.ts
@@ -0,0 +1,13 @@
+import { DataSource } from 'typeorm';
+import { Seeder, SeederFactoryManager } from 'typeorm-extension';
+
+export class WorkspaceSeeder1760696739409 implements Seeder {
+    track = false;
+
+    public async run(
+        dataSource: DataSource,
+        factoryManager: SeederFactoryManager
+    ): Promise<any> {
+
+    }
+}
