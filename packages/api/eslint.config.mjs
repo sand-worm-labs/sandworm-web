a92d738e5967a1bcfc6274080762c543fcd8c494
Ifechukwudaniel <mailidanielifechukwu@gmail.com>
2025-10-20 21:17:31 +0000

    feat: add comment, tag, and user follow entities with corresponding factories and migrations
    - Implemented CommentEntity with relationships to ArticleEntity and UserEntity.
    - Created TagEntity with unique name constraint.
    - Added UserFollowsEntity to manage follower-followee relationships.
    - Developed factories for Article, Comment, Tag, and User entities to facilitate seeding.
    - Created migrations for user, tag, article, comment, user_favorites, and user_follows tables.
    - Added seeders for users, tags, articles, comments, user follows, and user favorites.
    - Updated tsconfig for TypeScript configuration.
    - Enhanced turbo.json for improved task management and environment variable handling.

---
+++ packages/api/eslint.config.mjs
@@ -0,0 +1,3 @@
+import nest from '@sandworm/eslint-config/eslint-nest.config.mjs';
+
+export default [...nest];
---
+++ packages/api/src/dto/offset-pagination/page-options.dto.ts
@@ -0,0 +1,29 @@
+import { Order } from '@sandworm/nest-common';
+import { DEFAULT_PAGE_LIMIT } from '../../constants';
+import {
+  EnumFieldOptional,
+  NumberFieldOptional,
+  StringFieldOptional,
+} from '../../decorators';
+
+export class PageOptionsDto {
+  @NumberFieldOptional({
+    min: 1,
+    default: DEFAULT_PAGE_LIMIT,
+    int: true,
+  })
+  readonly limit: number = DEFAULT_PAGE_LIMIT;
+
+  @NumberFieldOptional({
+    min: 0,
+    default: 0,
+    int: true,
+  })
+  readonly offset: number = 0;
+
+  @StringFieldOptional()
+  readonly q?: string;
+
+  @EnumFieldOptional(() => Order, { default: Order.ASC })
+  readonly order: Order = Order.ASC;
+}
