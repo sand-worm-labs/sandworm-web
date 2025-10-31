8cb4187e0c4470c1d3623c7c3d4b23687decd86f
ceeriil <simonorugbo15@gmail.com>
2025-10-31 13:20:31 +0100

    ✨ feat: enhance loading animation and update blockchain icons with images

--- apps/web/src/components/Sections/SectionAI/Blockchains.tsx
+++ apps/web/src/components/Sections/SectionAI/Blockchains.tsx
@@ -1,14 +1,33 @@
 import React from "react";
+import Image from "next/image";
 
 export const Blockchains = () => {
   return (
     <div className="wrap">
-      <div className="ball"></div>
-      <div className="ball"></div>
-      <div className="ball"></div>
-      <div className="ball"></div>
-      <div className="ball"></div>
-      <div className="ball"></div>
+      <div className="ball">
+        <Image src="/img/eth.svg" alt="Ethereum" width={78} height={78} />
+      </div>
+      <div className="ball">
+        <Image src="/img/base.svg" alt="Ethereum" width={96} height={96} />
+      </div>
+      <div className="ball">
+        <Image src="/img/polygon.svg" alt="Ethereum" width={82} height={82} />
+      </div>
+      <div className="ball">
+        <Image src="/img/celo.svg" alt="Ethereum" width={76} height={76} />
+      </div>
+      <div className="ball">
+        <Image src="/img/op.svg" alt="Ethereum" width={72} height={72} />
+      </div>
+      <div className="ball">
+        <Image
+          src="/img/arbitrum.svg"
+          alt="Ethereum"
+          width={96}
+          height={96}
+          className="w-full object-cover"
+        />
+      </div>
     </div>
   );
 };
