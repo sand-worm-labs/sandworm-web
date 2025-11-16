5781915c21016682114e51a0360e2a11ae5fa498
ceeriil <simonorugbo15@gmail.com>
2025-11-16 19:17:57 +0100

    ✨ feat: enhance dark mode support across chat components and improve styling consistency

--- apps/web/src/components/Chats/MiniChat.tsx
+++ apps/web/src/components/Chats/MiniChat.tsx
@@ -9,31 +9,33 @@ export const MiniChat = () => {
     <div className="flex flex-col items-center w-full md:max-w-[800px] max-w-[calc(100dvw-32px)] px-4 md:px-0 mx-auto py-6">
       <div className="flex flex-col w-full gap-4 mb-6">
         <div className="flex justify-end">
-          <div className="bg-[#DEFCFE] text-[#343A40] px-4 py-2 rounded-2xl max-w-[75%] rounded-br-none ">
+          <div className="bg-[#DEFCFE] text-[#343A40] dark:bg-[#121417] px-4 py-2 rounded-2xl max-w-[75%] rounded-br-none dark:text-[#8696A6] ">
             Create a bar chart with tokens above $1m market cap on Base.
           </div>
         </div>
 
         <div className="flex justify-start flex-col mt-5">
-          <div className="max-w-[80%] text-[#343A40] mb-3 ">
+          <div className="max-w-[80%] text-[#343A40] dark:text-white mb-3 ">
             Here’s a bar chart on the different tokens above $1m market cap on
             Base.
           </div>
-          <div className="bg-[#F1F3F4] text-[#343A40] px-4 py-2 rounded-2xl max-w-[75%] rounded-bl-none ">
-            <div className="text-sm font-medium mb-2 text-[#0F0F0F]">Chart</div>
+          <div className="bg-[#F1F3F4] text-[#343A40] px-4 py-2 rounded-2xl max-w-[75%] rounded-bl-none dark:text-[#8696A6] dark:bg-[#121417] ">
+            <div className="text-sm font-medium mb-2 text-[#0F0F0F] ">
+              Chart
+            </div>
             Here’s a bar chart on the different tokens above $1m market cap on
             Base.
           </div>
         </div>
 
         <div className="flex justify-end mt-5">
-          <div className="bg-[#DEFCFE] text-[#343A40] px-4 py-2 rounded-2xl max-w-[75%] rounded-br-none ">
+          <div className="bg-[#DEFCFE] text-[#343A40] px-4 py-2 rounded-2xl max-w-[75%] rounded-br-none dark:text-[#8696A6] dark:bg-[#121417]  ">
             Ohhh that’s clean. So like Dune but cooler?
           </div>
         </div>
 
         <div className="flex justify-start ">
-          <div className="bg-[#F1F3F4] text-[#343A40]  px-4 py-2 rounded-2xl max-w-[75%] rounded-bl-none ">
+          <div className="bg-[#F1F3F4] text-[#343A40]  px-4 py-2 rounded-2xl max-w-[75%] rounded-bl-none dark:text-[#8696A6] dark:bg-[#121417]">
             Here’s a bar chart on the different tokens above $1m market cap on
             Base.
           </div>
