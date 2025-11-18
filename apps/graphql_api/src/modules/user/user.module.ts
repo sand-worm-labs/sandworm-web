9180aceeaf87f68a35967ddad76ed45d4c4afdff
Angry Coding <mailidanielifechukwu@gmail.com>
2025-11-18 19:15:35 +0000

    chore: user auth and delete scripts

--- apps/graphql_api/src/modules/user/user.module.ts
+++ apps/graphql_api/src/modules/user/user.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { UserEntity } from '@sandworm/postgresql-typeorm';
+import { UserEntity,UserFollowsEntity,UserSettingEntity } from '@sandworm/postgresql-typeorm';
 import { AuthModule } from '../auth/auth.module';
 import { UserResolver } from './user.resolver';
 import { UserService } from './user.service';
 @Module({
-  imports: [TypeOrmModule.forFeature([UserEntity]), AuthModule],
+  imports: [TypeOrmModule.forFeature([UserEntity,UserFollowsEntity,UserSettingEntity]), AuthModule],
   providers: [UserResolver, UserService],
 })
 export class UserModule { }
