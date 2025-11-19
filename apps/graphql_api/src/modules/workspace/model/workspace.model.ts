29b9c68a8c3aaa8dd058083a95520a6af9c013f8
Angry Coding <mailidanielifechukwu@gmail.com>
2025-11-19 18:47:09 +0000

    chore: model

--- apps/graphql_api/src/modules/workspace/model/workspace.model.ts
+++ apps/graphql_api/src/modules/workspace/model/workspace.model.ts
@@ -1,7 +1,7 @@
 import { Field, ObjectType, ID } from '@nestjs/graphql';
 import { User } from '../../user/model/user.model';
 import { Document } from '../../document/model/document.model';
-//import { OnboardingTutorial } from './onboarding_tutorial.model';
+import { OnboardingTutorial } from './onboarding_tutorial.model';
 import { Plan } from '@sandworm/postgresql-typeorm';
 
 @ObjectType()
@@ -24,15 +24,12 @@ export class Workspace {
   @Field(() => Plan)
   plan!: Plan;
 
-  @Field(() => ID)
-  ownerId!: string;
-
   @Field(() => User)
   owner!: User;
 
   @Field(() => [Document])
   documents!: Document[];
 
-  // @Field(() => [OnboardingTutorial])
-  // onboardingTutorials!: OnboardingTutorial[];
+  @Field(() => [OnboardingTutorial])
+  onboardingTutorials!: OnboardingTutorial[];
 }
