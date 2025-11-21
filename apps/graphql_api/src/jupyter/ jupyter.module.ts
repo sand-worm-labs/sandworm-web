24de60250cbf26b4ef87b1eafae3086869499362
Angry Coding <mailidanielifechukwu@gmail.com>
2025-11-21 01:51:51 +0000

    chore: jupyter  module

---
+++ apps/graphql_api/src/jupyter/ jupyter.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common'
+// import { JupyterService } from './jupyter.service'
+// import { SocketServerAdapter } from './jupyter.gateway'
+
+@Module({
+  providers: [],
+  exports: [],
+})
+export class JupyterModule {}
