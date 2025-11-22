290adc52aedea0a0080ea43c511551e3d8e8c82c
ceeriil <simonorugbo15@gmail.com>
2025-11-22 08:03:09 +0100

    ♻️ refactor: add universal stable keys, fix TS & ESLint issues, clean up Transition styling

--- packages/ui/src/lib/utils.ts
+++ packages/ui/src/lib/utils.ts
@@ -1,6 +1,24 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+import { useRef } from "react";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+/**
+ * Hook to generate stable keys for arrays of objects that don't have an ID.
+ * Returns a function that gives a stable key per object.
+ */
+export function useStableKeys<T extends object>() {
+  const keyMap = useRef<WeakMap<T, string>>(new WeakMap());
+
+  const getKey = (item: T) => {
+    if (!keyMap.current.has(item)) {
+      keyMap.current.set(item, crypto.randomUUID());
+    }
+    return keyMap.current.get(item)!;
+  };
+
+  return getKey;
 }
