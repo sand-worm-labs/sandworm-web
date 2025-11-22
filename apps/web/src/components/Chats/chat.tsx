6e361911085c68a0900b9c28d770a7ca40ae4480
ceeriil <simonorugbo15@gmail.com>
2025-11-22 09:39:30 +0100

    🔧 chore: add smooth glowing textarea on focus and file upload integration

--- apps/web/src/components/Chats/chat.tsx
+++ apps/web/src/components/Chats/chat.tsx
@@ -4,12 +4,9 @@ import type { Attachment, Message } from "ai";
 import { useChat } from "ai/react";
 import { useState } from "react";
 
-/* import { Message as PreviewMessage } from "@/components/Chats/message";
- */
 import { MultimodalInput } from "./multimodal-input";
 import { ExamplePrompts } from "./example-prompts";
-/* import { useScrollToBottom } from "./use-scroll-to-bottom";
- */
+
 export function Chat({
   id,
   initialMessages,
@@ -17,7 +14,6 @@ export function Chat({
   id: string;
   initialMessages: Array<Message>;
 }) {
-  console.log("id", id);
   const { messages, input, setInput, append, isLoading, stop, handleSubmit } =
     useChat({
       id,
@@ -35,33 +31,11 @@ export function Chat({
     });
 
   const [attachments, setAttachments] = useState<Array<Attachment>>([]);
-  /*   const [messagesContainerRef, messagesEndRef] =
-    useScrollToBottom<HTMLDivElement>(); */
 
   return (
-    <div className="flex flex-row justify-center pb-4 md:pb-8 h-dvh ">
-      <div className="flex flex-col mt-32 items-center gap-4">
-        {/*   <div
-          ref={messagesContainerRef}
-          className="flex flex-col gap-4 h-full w-dvw items-center overflow-y-scroll"
-        >
-          {messages.map(message => (
-            <PreviewMessage
-              key={message.id}
-              chatId={id}
-              role={message.role}
-              content={message.content}
-              attachments={message.experimental_attachments}
-              toolInvocations={message.toolInvocations}
-            />
-          ))}
-
-          <div
-            ref={messagesEndRef}
-            className="shrink-0 min-w-[24px] min-h-[24px]"
-          />
-        </div> */}
-        <h1 className="text-3xl xl:text-4xl font-medium text-center tracking-tighter text-pretty roobert">
+    <div className="flex flex-row justify-center pb-4 md:pb-8 h-dvh bg-[#F9FAFD] dark:bg-black ">
+      <div className="flex flex-col mt-32 items-center gap-2">
+        <h1 className="text-3xl lg:text-3xl font-medium text-center tracking-tighter text-pretty font-primary">
           What do you want to explore onchain today?
         </h1>
 
