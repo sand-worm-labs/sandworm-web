31566206f539e75ee7c7f2fa12b49422bfb32a11
ceeriil <simonorugbo15@gmail.com>
2025-11-25 14:47:36 +0100

    chore: update ESLint rules

--- apps/web/src/components/Visualization/blocks/customBlocks/CodeEditor/theme.ts
+++ apps/web/src/components/Visualization/blocks/customBlocks/CodeEditor/theme.ts
@@ -39,7 +39,7 @@ function materialLightTheme(disabled: boolean) {
 }
 
 const materialLightHighlightStyle = HighlightStyle.define([
-  { tag: t.comment, color: "#008000" },
+  { tag: t.comment, color: "#C7665C" },
   { tag: t.keyword, color: "#0000ff" },
   { tag: t.number, color: "#098658" },
   { tag: t.string, color: "#a31515" },
