948495ee7c4d5bde7a1158d944ee63c4f760bf44
Angry Coding <mailidanielifechukwu@gmail.com>
2025-11-25 04:13:26 +0000

    chore: schedule

---
+++ apps/graphql_api/src/modules/schedule/schedule.module.ts
@@ -0,0 +1,17 @@
+// apps/graphql_api/src/modules/schedule/schedule.module.ts
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ExecutionScheduleEntity, DocumentEntity } from '@sandworm/postgresql-typeorm';
+import { AuthModule } from '../auth/auth.module';
+import { ScheduleResolver } from './schedule.resolver';
+import { ScheduleService } from './schedule.service';
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([ExecutionScheduleEntity, DocumentEntity]),
+    AuthModule,
+  ],
+  providers: [ScheduleResolver, ScheduleService],
+  exports: [ScheduleService],
+})
+export class ScheduleModule {}
