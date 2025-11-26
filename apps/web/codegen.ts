b4d43693047d3248a9ab5cecfbc706e8dd95d953
Angry Coding <mailidanielifechukwu@gmail.com>
2025-11-26 04:22:45 +0000

    chore: fixes

---
+++ apps/web/codegen.ts
@@ -0,0 +1,25 @@
+import type { CodegenConfig } from "@graphql-codegen/cli";
+import dotenv from "dotenv";
+
+dotenv.config({ path: ".env.local" });
+
+const graphqlCodegenConfig: CodegenConfig = {
+  schema: "http://localhost:8003/graphql",
+  documents: "./schema.gql",
+  generates: {
+    "src/generated/graphql.ts": {
+      plugins: [
+        "typescript",
+        "typescript-operations",
+        "typescript-react-apollo",
+      ],
+      config: {
+        withHooks: true,
+        withHOC: false,
+        withComponent: false,
+      },
+    },
+  },
+};
+
+export default graphqlCodegenConfig;
