bafd1e7a302e53c5b657b3617f79269c63002be5
ceeriil <simonorugbo15@gmail.com>
2025-11-30 10:27:48 +0100

    chore:refactor layout to integrate AppHeader in workspace views

--- apps/web/src/components/Layout/AppHeader/index.tsx
+++ apps/web/src/components/Layout/AppHeader/index.tsx
@@ -1,11 +1,9 @@
 "use client";
 
-import Link from "next/link";
 import { useState, type FC } from "react";
 import { useSession } from "next-auth/react";
 import { X, Search } from "lucide-react";
 
-import { SandwormLogo } from "@/components/Assets";
 import { ThemeTogggle } from "@/components/Theme/ThemeToggle";
 
 import { ProfileMenu } from "../../ProfileMenu";
@@ -17,13 +15,7 @@ export const AppHeader: FC = () => {
 
   return (
     <header className="bg-background px-8 py-2 flex justify-between items-center border-b border-[#E9ECEF] dark:border-[#262A30] ">
-      <Link href="/" className="flex items-center ">
-        <SandwormLogo />
-        <span className="ml-3 font-bold text-lg uppercase hidden md:inline-block">
-          SandW0rm.
-        </span>
-      </Link>
-
+      <span className="text-lg font-medium">Home</span>
       <div className="hidden md:block">
         <SearchBar />
       </div>
