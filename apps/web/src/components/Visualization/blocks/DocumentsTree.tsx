d403741004cda3ced10202fa86d2dcf456d64efc
ceeriil <simonorugbo15@gmail.com>
2025-11-30 06:48:21 +0100

    chore: refactor chat component to improve project creation and submission handling

--- apps/web/src/components/Visualization/blocks/DocumentsTree.tsx
+++ apps/web/src/components/Visualization/blocks/DocumentsTree.tsx
@@ -630,7 +630,7 @@ function DropDown(props: DropDownProps) {
                         hidden: !props.isFavoriteDropdown,
                       },
                       active ? "bg-gray-100 text-gray-900" : "text-gray-700",
-                      "w-full px-4 py-2 text-left text-sm flex items-center gap-x-2 font-primary"
+                      "w-full px-4 py-2 text-left text-sm flex items-center gap-x-2 font-primary  "
                     )}
                   >
                     <BookmarkSlashIcon className="h-4 w-4" />
