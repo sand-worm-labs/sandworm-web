2152ed97c7eefdc89accd78d7718b0acb6b31ad2
ceeriil <simonorugbo15@gmail.com>
2025-12-12 17:32:59 +0100

    refactor: enhance component structure across various files

--- apps/web/src/components/AuthUI/SignInForm.tsx
+++ apps/web/src/components/AuthUI/SignInForm.tsx
@@ -1,8 +1,9 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import { useLogin, tokenStorage } from "../Visualization/hooks/useAuth";
 import { useRouter, useSearchParams } from "next/navigation";
+
+import { useLogin, tokenStorage } from "../Visualization/hooks/useAuth";
 
 export const SignInForm = () => {
   const router = useRouter();
