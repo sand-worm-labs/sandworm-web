c171aa89819645c8b19e298292f35299da163560
Angry Coding <mailidanielifechukwu@gmail.com>
2025-12-17 11:05:25 +0000

    feat: enhance Auth module with user roles and improve structure

--- apps/api/src/modules/auth-graphql/auth-graphql.resolver.ts
+++ apps/api/src/modules/auth-graphql/auth-graphql.resolver.ts
@@ -1,12 +1,16 @@
-import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Parent, ResolveField, Resolver } from '@nestjs/graphql';
 import { Public } from '@sandworm/nest-common';
 import { AuthGraphqlService } from './auth-graphql.service';
 import { LoginInput } from './dto/auth-graphql.dto';
 import { AuthPayload } from './models/auth-payload';
+import { UserService } from '../user/user.service';
+import GraphQLJSON from 'graphql-type-json';
 
 @Resolver()
 export class AuthGraphqlResolver {
-  constructor(private authService: AuthGraphqlService) {}
+  constructor(
+    private authService: AuthGraphqlService
+  ) { }
 
   @Public()
   @Mutation(() => AuthPayload, { name: 'login', description: 'Sign in' })
