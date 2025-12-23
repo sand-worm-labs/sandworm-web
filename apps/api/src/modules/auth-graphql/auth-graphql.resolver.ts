a5e00d345c2ad2803260f450b32c7025bb59ed77
Angry Coding <mailidanielifechukwu@gmail.com>
2025-12-23 13:04:43 +0000

    feat: implement WebSocket functionality with connection handling and workspace management

--- apps/api/src/modules/auth-graphql/auth-graphql.resolver.ts
+++ apps/api/src/modules/auth-graphql/auth-graphql.resolver.ts
@@ -1,10 +1,8 @@
-import { Args, Mutation, Parent, ResolveField, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
 import { Public } from '@sandworm/nest-common';
 import { AuthGraphqlService } from './auth-graphql.service';
 import { LoginInput } from './dto/auth-graphql.dto';
 import { AuthPayload } from './models/auth-payload';
-import { UserService } from '../user/user.service';
-import GraphQLJSON from 'graphql-type-json';
 
 @Resolver()
 export class AuthGraphqlResolver {
