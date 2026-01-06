b2edf9f42df3c91c86b7105a1ed536cfaccee468
Angry Coding <mailidanielifechukwu@gmail.com>
2026-01-06 15:35:55 +0000

    fixes

--- apps/api/src/code-executor/python-executor.service.ts
+++ apps/api/src/code-executor/python-executor.service.ts
@@ -65,13 +65,4 @@ export class PythonExecutorService {
         };
     }
 
-    private escapePythonString(str: string): string {
-        return str
-            .replace(/\\/g, '\\\\')
-            .replace(/'/g, "\\'")
-            .replace(/\n/g, '\\n')
-            .replace(/\r/g, '\\r')
-            .replace(/\t/g, '\\t');
-    }
-
 }
