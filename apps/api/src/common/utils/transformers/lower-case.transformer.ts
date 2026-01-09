4682993964a2ecc610da5259d2f5dfc23c6b17df
Angry Coding <mailidanielifechukwu@gmail.com>
2026-01-09 23:09:59 +0000

    refactor: flatten common directory structure and standardize import paths

--- apps/api/src/common/utils/transformers/lower-case.transformer.ts
+++ apps/api/src/common/utils/transformers/lower-case.transformer.ts
@@ -1,5 +1,5 @@
 import { TransformFnParams } from 'class-transformer/types/interfaces';
-import { MaybeType } from '../types/maybe.type';
+import { MaybeType } from '@/common/types/maybe.type';
 
 export const lowerCaseTransformer = (
   params: TransformFnParams,
