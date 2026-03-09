65817918686713d7a89d36242917ee157d103931
ceeriil <simonorugbo15@gmail.com>
2026-03-09 22:11:10 +0100

    chore: proxy api url

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -8,6 +8,12 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  rewrites: async () => [
+    {
+      source: "/api/:path*",
+      destination: "http://192.168.1.76:8080/:path*",
+    },
+  ],
   images: {
     domains: [
       "cryptologos.cc",
