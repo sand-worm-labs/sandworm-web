db3363d99ebce4fe564d5e85473db939f14032f2
ifechukwudaniel.eth <mailidanielifechukwu@gmail.com>
2026-03-25 15:01:36 +0000

    feat(env-vars): encrypt environment variable values
    - Encrypts EnvironmentVariableEntity values
    - Uses transformer for seamless encryption
    - Renames onWriteback to onPowerToolbox

--- packages/postgresql-typeorm/src/entities/environment_variable.entity.ts
+++ packages/postgresql-typeorm/src/entities/environment_variable.entity.ts
@@ -5,40 +5,48 @@ import {
     JoinColumn,
     PrimaryGeneratedColumn,
     Unique,
-  } from "typeorm";
-  import type { Relation } from "typeorm";
-  import { AbstractEntity } from "./abstract.entity";
-  import { WorkspaceEntity } from "./workspace.entity";
-  
-  @Entity("environment_variable")
-  @Unique("unique_workspaceId_name", ["workspaceId", "name"])
-  export class EnvironmentVariableEntity extends AbstractEntity {
+} from "typeorm";
+import type { Relation } from "typeorm";
+import { AbstractEntity } from "./abstract.entity";
+import { WorkspaceEntity } from "./workspace.entity";
+import { encrypt, decrypt } from "@sandworm/nest-common";
+
+@Entity("environment_variable")
+@Unique("unique_workspaceId_name", ["workspaceId", "name"])
+export class EnvironmentVariableEntity extends AbstractEntity {
     constructor(data?: Partial<EnvironmentVariableEntity>) {
-      super();
-      Object.assign(this, data);
+        super();
+        Object.assign(this, data);
     }
-  
+
     @PrimaryGeneratedColumn("uuid", {
-      primaryKeyConstraintName: "PK_environment_variable_id",
+        primaryKeyConstraintName: "PK_environment_variable_id",
     })
     id!: string;
-  
+
     @Column({ name: "name", type: "varchar" })
     name!: string;
-  
-    @Column({ name: "value", type: "varchar" })
+
+    @Column({
+        name: "value",
+        type: "varchar",
+        transformer: {
+            to: (value: string) => encrypt(value, process.env.ENVIRONMENT_VARIABLES_ENCRYPTION_KEY),
+            from: (value: string) => decrypt(value, process.env.ENVIRONMENT_VARIABLES_ENCRYPTION_KEY),
+        },
+    })
     value!: string;
-  
+
     @Column({ name: "workspace_id", type: "uuid" })
     workspaceId!: string;
-  
+
     @ManyToOne(() => WorkspaceEntity, (workspace) => workspace.environmentVariables, {
-      onDelete: "CASCADE",
+        onDelete: "CASCADE",
     })
     @JoinColumn({
-      name: "workspace_id",
-      referencedColumnName: "id",
-      foreignKeyConstraintName: "FK_environment_variable_workspace",
+        name: "workspace_id",
+        referencedColumnName: "id",
+        foreignKeyConstraintName: "FK_environment_variable_workspace",
     })
     workspace!: Relation<WorkspaceEntity>;
-  }
+}
