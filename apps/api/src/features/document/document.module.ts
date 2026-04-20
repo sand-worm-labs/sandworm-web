1b9817227f4703180258059f979a3e137bb54512
ifechukwudaniel.eth <mailidanielifechukwu@gmail.com>
2026-04-20 11:58:13 +0000

    feat(postgresql-typeorm): export DocumentFork entity
    - Added export for document_fork.entity

--- apps/api/src/features/document/document.module.ts
+++ apps/api/src/features/document/document.module.ts
@@ -1,6 +1,6 @@
 import { forwardRef, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { DocumentEntity, WorkspaceEntity, FavoriteEntity, YjsDocumentEntity } from '@sandworm/postgresql-typeorm';
+import { DocumentEntity, WorkspaceEntity, FavoriteEntity, YjsDocumentEntity, DocumentForkEntity } from '@sandworm/postgresql-typeorm';
 import { DocumentResolver } from './document.resolver';
 import { DocumentService } from './service/document.service';
 import { DocumentTreeService } from './service/document-tree.service';
@@ -9,7 +9,7 @@ import { YjsModule } from '../collaboration/yjs/yjs.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([DocumentEntity, WorkspaceEntity, FavoriteEntity, YjsDocumentEntity]),
+    TypeOrmModule.forFeature([DocumentEntity, DocumentForkEntity, WorkspaceEntity, FavoriteEntity, YjsDocumentEntity]),
     AuthGraphqlModule,
      forwardRef(() => YjsModule)
   ],
