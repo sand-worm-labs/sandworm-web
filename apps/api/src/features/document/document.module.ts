0f6a2a05eb8f690f7c192f7fb6574b29f5460913
ifechukwudaniel.eth <mailidanielifechukwu@gmail.com>
2026-04-20 22:42:15 +0000

    feat(document): add author field and featured documents query
    - Expose author details via Document type
    - Add getFeaturedDocuments GraphQL query

--- apps/api/src/features/document/document.module.ts
+++ apps/api/src/features/document/document.module.ts
@@ -6,12 +6,14 @@ import { DocumentService } from './service/document.service';
 import { DocumentTreeService } from './service/document-tree.service';
 import { AuthGraphqlModule } from '../auth/graphql/auth-graphql.module';
 import { YjsModule } from '../collaboration/yjs/yjs.module';
+import { UserModule } from '../user/user.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([DocumentEntity, DocumentForkEntity, WorkspaceEntity, FavoriteEntity, YjsDocumentEntity]),
     AuthGraphqlModule,
-     forwardRef(() => YjsModule)
+    forwardRef(() => YjsModule),
+    UserModule
   ],
   providers: [DocumentResolver, DocumentService, DocumentTreeService],
   exports: [DocumentService, DocumentTreeService]
