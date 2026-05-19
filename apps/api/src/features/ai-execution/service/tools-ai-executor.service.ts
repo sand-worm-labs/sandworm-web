f79fafdc5b55d3c008cb7d2f72a23cf85f10dc76
ifechukwudaniel.eth <mailidanielifechukwu@gmail.com>
2026-05-19 13:00:03 +0000

    chore: xhages

--- apps/api/src/features/ai-execution/service/tools-ai-executor.service.ts
+++ apps/api/src/features/ai-execution/service/tools-ai-executor.service.ts
@@ -1,7 +1,18 @@
 import { Injectable, Logger } from '@nestjs/common'
 import { BaseAiExecutorService} from './base-ai-executor.service'
+import { YjsDocumentService } from '@/features/collaboration/yjs/yjs-document.service';
+import { PersistorFactory } from '@/features/collaboration/yjs/persistors/persistor.factory';
 
 @Injectable()
 export class ToolsAiExecutorService extends BaseAiExecutorService {
   protected readonly logger = new Logger(ToolsAiExecutorService.name)
+
+
+  constructor(
+    yjsDocumentService: YjsDocumentService,
+     persistorFactory: PersistorFactory,  
+  ) {
+    super(yjsDocumentService, persistorFactory);
+  }
+
 }
--- apps/api/src/features/ai-execution/service/visualization-ai-executor.service.ts
+++ apps/api/src/features/ai-execution/service/visualization-ai-executor.service.ts
@@ -1,8 +1,17 @@
 import { Injectable, Logger } from '@nestjs/common'
 import { BaseAiExecutorService } from './base-ai-executor.service'
+import { YjsDocumentService } from '@/features/collaboration/yjs/yjs-document.service';
+import { PersistorFactory } from '@/features/collaboration/yjs/persistors/persistor.factory';
 
 @Injectable()
 export class VisualizationAiExecutorService extends BaseAiExecutorService {
   protected readonly logger = new Logger(VisualizationAiExecutorService.name)
+
+  constructor(
+    yjsDocumentService: YjsDocumentService,
+     persistorFactory: PersistorFactory,  
+  ) {
+    super(yjsDocumentService, persistorFactory);
+  }
 
 }
